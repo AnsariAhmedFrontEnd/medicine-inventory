079209a85dd20163559f68c9a85bc18d9b492b93
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-07-28 15:43:40 +0530

    Added Data Components

---
+++ src/components/MedicineItems.js
@@ -0,0 +1,9 @@
+const MedicineItems = () => {
+    return (
+        <div>
+            Here will come the medicineDate;
+        </div>
+    )
+};
+
+export default MedicineItems;
---
+++ src/store/MedicineProveider.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import MedicineContext from "./medicine-context";
+
+const MedicineProvider = (props) => {
+    const [medicineItems, setMedicineItems] = useState([]);
+const addProductHandler = (item) => {
+    setMedicineItems(preveiousItems => [...preveiousItems, item])
+
+};
+
+    const medicineContext = {
+        medicineItems : medicineItems,
+        addProduct:addProductHandler
+    }
+
+   return (
+  <MedicineContext.Provider value={medicineContext}>{props.children}</MedicineContext.Provider>)
+};
+
+export default MedicineProvider;
